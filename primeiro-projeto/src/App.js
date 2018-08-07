a800fb00e368108b92c78d3b01b311833649ebf2
Alberto Verzemiassi <betoverzemiassi@gmail.com>
2018-08-07 10:32:36 -0300

    Adicionando material de apoio, ciclo de vida de um componente

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,24 +1,131 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Ola from './Ola';
+import axios from 'axios';
+import api from './Api';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-		<p>Olá pessoal da série de vídeos sobre React</p>
-		<Ola name='Alberto'/>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			genres: [],
+			isLoading: false
+		}
+	}
+	//Componente seu ciclo de vida
+	componentDidMount() {
+		// setInterval(() => this.setState({
+		// 	count: this.state.count+1
+		// }), 1000)
+		this.setState({ isLoading: true })//carrega os dados
+		api.loadGenres()
+			.then((res)=> {//só vai acontecer quando os dados terminar de ser carregados
+				this.setState({
+					isLoading: false,
+					genres: res.data
+				})
+			})
+	}
+
+	renderGenreLink(genre) {
+		return (
+			<span>&nbsp;<a href=''>{genre}</a></span>
+		)
+	}
+	render() {
+		return (
+			<div>
+				<nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+						<div className="container">
+							<div className="navbar-header page-scroll">
+							<a className="navbar-brand page-scroll" href="#page-top">
+									<img src="images/logo.png" height="30" />
+							</a>
+							</div>
+
+							<div className="collapse navbar-collapse navbar-ex1-collapse">
+							<ul className="nav navbar-nav">
+								<li>
+									<a href="">Menu item {this.state.count}</a>
+								</li>
+							</ul>
+							</div>
+
+						</div>
+				</nav>
+
+				<section id="intro" className="intro-section">
+						<div className="container">
+							<div className="row">
+							<div className="col-lg-12">
+								<h1><img src="images/logo.png" /></h1>
+								<p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+							</div>
+							</div>
+						</div>
+				</section>
+
+				<section id="services" className="services-section">
+						<div className="container">
+							<div className="row">
+							<div className="col-lg-12">
+								<h1>Para assistir</h1>
+								<div id="series" className="row list-group">
+									<div className="item  col-xs-4 col-lg-4">
+									<div className="thumbnail">
+										<img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+										<div className="caption">
+											<h4 className="group inner list-group-item-heading">
+											How I met your mother</h4>
+											<div className="row">
+											<div className="col-xs-12 col-md-6">
+												<p className="lead">
+													AÇÃO</p>
+											</div>
+											<div className="col-xs-12 col-md-6">
+												<a className="btn btn-success" href="">Gerenciar</a>
+											</div>
+											</div>
+										</div>
+									</div>
+									</div>
+									<div className="item  col-xs-4 col-lg-4">
+									<div className="thumbnail">
+										<img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+										<div className="caption">
+											<h4 className="group inner list-group-item-heading">
+											How I met your mother</h4>
+											<div className="row">
+											<div className="col-xs-12 col-md-6">
+												<p className="lead">
+													AÇÃO</p>
+											</div>
+											<div className="col-xs-12 col-md-6">
+												<a className="btn btn-success" href="http://www.jquery2dotnet.com">Gerenciar</a>
+											</div>
+											</div>
+										</div>
+									</div>
+									</div>
+								</div>
+							</div>
+							</div>
+						</div>
+				</section>
+				<section>
+					{
+						this.state.isLoading &&
+						<span>Aguarde, carregando...</span>
+					}
+					{	!this.state.isLoading &&
+						<div>
+							Ver séries do genêro:
+							{this.state.genres.map(this.renderGenreLink)}
+						</div>
+					}
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
